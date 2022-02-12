ffe62e1afadd951cfcdf73d215b1777a2c7571e7
Tim4ukys <73384372+Tim4ukys@users.noreply.github.com>
2022-02-12 21:46:34 +0700

    fix

--- 05/main.cpp
+++ 05/main.cpp
@@ -5,12 +5,25 @@ int main() {
     using std::cout;
     using std::endl;
 
-    std::uint64_t n;
+    std::uint64_t n, ans = 1, x2 = 1, x3, x5;
     cin >> n;
 
-    while (n % 2 && n % 3 && n % 5) { n--; }
+    while (x2 <= n) {
+        x3 = x2;
+        while (x3 <= n) {
+            x5 = x3;
+            while (x5 <= n)
+            {
+                ans = std::max(ans, x5);
+                x5 *= 5;
+            }
+            x3 *= 3;
+        }
+        x2 *= 2;
+    }
 
-    cout << n << endl;
+
+    cout << ans << endl;
 
     return 0;
 }
